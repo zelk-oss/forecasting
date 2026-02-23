a421800bf022dbe00cea80eddf5ee211929e0d8d
Chiara ZELCO <chiara.zelco@enpc.fr@kaitain>
2026-02-23 15:13:53 +0100

    studying training code

--- .ipynb_checkpoints/flow_unconditional-checkpoint.py
+++ .ipynb_checkpoints/flow_unconditional-checkpoint.py
@@ -36,7 +36,7 @@ n_heads = 4      # must divide n_features
 n_embedding = 16
 
 wave_length = 0.1
-lr = 3e-3
+lr = 1e-4
 
 # load data 
 ds_train = xr.open_zarr("../data/sqg_train.zarr")["q"].compute(num_workers=2)
