42c32a5e0f555c810bb081d7524d6e04bc24cb60
Chiara ZELCO <chiara.zelco@enpc.fr@kaitain>
2026-02-24 16:23:25 +0100

    changed dataset, now frames are more different from each other

--- .ipynb_checkpoints/constants-checkpoint.py
+++ .ipynb_checkpoints/constants-checkpoint.py
@@ -4,15 +4,15 @@ Constants obtained from compute_norms.py which reads dataset
 import torch
 
 
-__all__ = ["in_mean", "in_std", "res_mean", "res_std", "weights_lat"]
+__all__ = ["in_mean", "in_std", "res_mean", "res_std"]
 
 
 # Normalisation constants for the doubly-periodic single-channel dataset
-in_mean = torch.tensor([0.000000])[:, None, None]
-in_std  = torch.tensor([0.295010])[:, None, None]
+# coming from dataset with correct model time - days conversion 
+in_mean = torch.tensor([-0.000000])[:, None, None]
+in_std  = torch.tensor([0.302901])[:, None, None]
 res_mean= torch.tensor([-0.000000])[:, None, None]
-res_std = torch.tensor([0.009835])[:, None, None]
+res_std = torch.tensor([0.124247])[:, None, None]
 # Dataset is now doubly-periodic and no latitude-longitude reweighting
 # is required for training. Use scalar unity so losses remain unchanged
 # but broadcasting is trivial in the training loop.
-weights_lat = torch.tensor(1.0)
